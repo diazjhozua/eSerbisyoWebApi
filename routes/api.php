9ceefb0d9ca8fd21bf57666fa94cc205e5ec3a7b
DiamondShy <tenaciousshyo@gmail.com>
2021-08-29 23:17:36 +0800

    get
    get

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FeedbackController;
 use App\Http\Controllers\Api\DocumentTypeController;
 use App\Http\Controllers\Api\DocumentController;
-
+use App\Http\Controllers\APi\OrdinanceCategoryController;
 use App\Http\Controllers\Api\TermController;
 use App\Http\Controllers\Api\PositionController;
 use App\Http\Controllers\Api\EmployeeController;
 use App\Http\Controllers\Api\MissingPersonController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +32,7 @@ Route::resource('feedbacks', FeedbackController::class)->only(['index', 'store']
 Route::resource('document-types', DocumentTypeController::class)->except(['create']);
 Route::resource('documents', DocumentController::class)->except(['show']);
 
+Route::resource('ordinance-categories', OrdinanceCategoryController::class);
 
 Route::resource('terms', TermController::class)->except(['create']);
 Route::resource('positions', PositionController::class)->except(['create']);
