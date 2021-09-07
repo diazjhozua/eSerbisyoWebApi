4bade6b6fc0a279769ef154468640fcee9efd5a1
Jhozua Manguera Diaz <diaz.jhozua@gmail.com>
2021-09-07 15:16:38 +0800

    hehe

--- app/Http/Requests/ComplainantRequest.php
+++ app/Http/Requests/ComplainantRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\Api\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ComplainantRequest extends FormRequest
 {
@@ -23,9 +24,26 @@ class ComplainantRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'complaint_id' => 'required|integer|exists:complaints,id',
-            'name' => 'required|string|min:5|max:150|unique:complainants,name'.$this->complainant_id
-        ];
+
+        if ($this->isMethod('POST')) {
+            $complaint_id = $this->get('complaint_id');
+            return [
+                'complaint_id' => 'required|integer|exists:complaints,id',
+                'name' => ['required', 'string', 'min:5', 'max:150', Rule::unique('complainants', 'name')->where(function ($query) use ($complaint_id) {
+                    return $query->where('complaint_id', $complaint_id);
+                })],
+                'signature' => 'required|mimes:jpeg,png|max:3000',
+            ];
+        }
+
+        if ($this->isMethod('PUT')) {
+            $complaint_id = $this->get('complaint_id');
+            $id = $this->get('id');
+            dd($id);
+            return [
+                'id' => 'required|integer|exists:complainants,id',
+                'signature' => 'sometimes|mimes:jpeg,png|max:3000',
+            ];
+        }
     }
 }
