2569743b710a2a51fe93d49a48911803205307cd
Jhozua Manguera Diaz <diaz.jhozua@gmail.com>
2022-02-25 14:01:38 +0800

    Update TelescopeServiceProvider.php

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,15 +21,16 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local')) {
-                return true;
-            }
+            return true;
+            // if ($this->app->environment('local')) {
+            //     return true;
+            // }
 
-            return $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
+            // return $entry->isReportableException() ||
+            //        $entry->isFailedRequest() ||
+            //        $entry->isFailedJob() ||
+            //        $entry->isScheduledTask() ||
+            //        $entry->hasMonitoredTag();
         });
     }
 
